efab906bfb106a79f47f2c3fe758fa259033e976
santu <tanneerkumar@gmail.com>
2016-11-08 21:03:03 -0800

    code for 4.1.2

---
+++ 4.12.py
@@ -0,0 +1,16 @@
+#!/usr/bin/py
+
+x = raw_input ("enter a string:");
+
+if x=="one":
+    print "alert: 1"
+    elif x=="two":
+        print  "alert: 2"
+	elif x=="three":
+	    print "alert: 3"
+	    elif x=="four":
+	        print "alert: 4"
+		elif x=="five":
+		    print "alert: 5"
+		    else:
+		        print " please enter a valid number"
