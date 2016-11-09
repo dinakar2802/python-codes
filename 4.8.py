bb134a970359978f653af6440559bcd44b60503b
dinakar2802 <dinakar2802@gmail.com>
2016-11-08 19:30:01 -0800

    code for 4.8

---
+++ 4.8.py
@@ -0,0 +1,14 @@
+#! usr/bin/env/ python
+x = input ("Enter a value:")
+if (x==123):
+    print "alert:'One Two Three'"
+    elif (x==124):
+        print "alert:'One Two Four'"
+	elif (x==125):
+	    print "alert:'One Two Five'"
+	    elif(x==126):
+	        print "alert:'One Two Six'"
+		elif(x==127):
+		    print "alert:'One Two Seven'"
+		    else:
+		            print "alert:Enter a valid number"
