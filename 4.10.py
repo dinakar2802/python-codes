8769e44ed80cccbb3ff92e2fee508f83d3c88366
dinakar2802 <dinakar2802@gmail.com>
2016-11-08 20:05:55 -0800

    code for 4.10

---
+++ 4.10.py
@@ -0,0 +1,14 @@
+#! usr/bin/env/ python
+x = input ("Enter a value:")
+if (x>=1 and x<=10):
+    print "The number is between 1 and 10"
+    elif (x>=11 and x<=20):
+        print "The number is between 11 and 20"
+	elif (x>=21 and x<=30):
+	    print "The number is between 21 and 30"
+	    elif(x>=31 and x<=40):
+	        print "The number is between 31 and 40"
+		elif(x>=41 and x<=50):
+		    print "The number is between 41 and 50"
+		    else:
+		            print "alert:Please enter a valid number"
